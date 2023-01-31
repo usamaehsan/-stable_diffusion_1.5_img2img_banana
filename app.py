546b49ce5fff87f8f3b9b7b871c24b0f184982c1
usamaClick <53894527+usamaehsan@users.noreply.github.com>
2023-01-31 16:00:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,7 @@ def make_scheduler(name, config):
 def init():
     global model
     HF_AUTH_TOKEN = os.getenv('HF_AUTH_TOKEN')
-    model = StableDiffusionImg2ImgPipeline.from_pretrained(
-        model_path,
-        revision="fp16",
-        torch_dtype=torch.float16,
-        use_auth_token=HF_AUTH_TOKEN
-    )
+    model = StableDiffusionImg2ImgPipeline.from_pretrained(model_path, revision="fp16", torch_dtype=torch.float16, use_auth_token=HF_AUTH_TOKEN)
 
 
 def inference(model_inputs):
